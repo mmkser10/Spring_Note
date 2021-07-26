aadf81428a4ae842f86de86d080e1629b4b55aa3
Oflo <mmkseoj@gmail.com>
2021-07-26 20:16:47 +0900

    [core] refactor : AppConfig 리펙토링
    역할 및 구현클래스가 한눈에 들어오고 구현체 변경이 단순화 됨

--- spring-core/src/main/java/com/oflo/springcore/AppConfig.java
+++ spring-core/src/main/java/com/oflo/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package com.oflo.springcore;
 
+import com.oflo.springcore.discount.DiscountPolicy;
 import com.oflo.springcore.discount.FixDiscountPolicy;
+import com.oflo.springcore.member.MemberRepository;
 import com.oflo.springcore.member.MemberService;
 import com.oflo.springcore.member.MemberServiceImpl;
 import com.oflo.springcore.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.oflo.springcore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     };
 
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
